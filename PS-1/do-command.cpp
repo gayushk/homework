999dbb271cbfc3787b17e0a0d587e9f6598c5bf0
gayane <the.gay@bk.ru>
2025-11-03 01:44:19 +0400

    Update do-command.cpp

--- PS-1/do-command.cpp
+++ PS-1/do-command.cpp
@@ -2,29 +2,34 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 void do_command(char** argv) {
-	int argc = 0;
-	while(argv[argc] != nullptr) {
-	       ++argc;
-	}
+	struct timeval start, end;
+	gettimeofday(&start, nullptr);
 	pid_t pid = fork();
 	if(pid < 0) {
 		std::cerr << "error fork" << std::endl;
-		exit(EXIT_FAILURE);
+		exit(1);
 	} else if(pid == 0) {
 		execvp(argv[0], argv);
-		std::cerr << "error command" << std::endl;
-		exit(EXIT_FAILURE);
+		std::cerr << "error execvp" << std::endl;
+		exit(127);
 	} else {
 		int status;
-		wait(&status);
+		if(waitpid(pid, &status, 0) == -1) {
+			std::cerr << "error waitpid" << std::endl;
+			exit(1);
+		}
+		gettimeofday(&end, nullptr);
+		double t = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0
 		int exit_c = 0;
 		if(WIFEXITED(status)) {
 			exit_c = WEXITSTATUS(status);
-		} else if(WIFSIGNALED(status)) {
-			exit_c = 128 + WTERMSIG(status);
-		}	
+		} else {
+			exit_c = -1;
+		}
+		std::cout << "Command completed with " << exit_c << "exit code and took" << t << "seconds." << std::endl;
 	}
 }
 
