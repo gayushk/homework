aab08210908432404a33ddbd2cc13ff4f789c27c
gayane <the.gay@bk.ru>
2025-12-07 19:17:36 +0000

    file added

--- IPC-2/prime-calculator.cpp
+++ IPC-2/prime-calculator.cpp
@@ -1,79 +1,115 @@
 #include <iostream>
-#include <ctime>
-#include <random>
-#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <vector>
+#include <sys/wait.h>
 
-class ThreadClass {
-public:
-    pthread_t tid;
-    int* arr;
-    int start;
-    int end;
-    int res;
-};
+#define buffSize 100
 
-void* ths_sum(void* arg){
-    ThreadClass* tc = (ThreadClass*) arg;
-    tc->res = 0;
-    for(int i = tc->start; i < tc->end; i++) {
-        tc->res += tc->arr[i];
+bool checkPrime(int n) {
+    if (n % 2 == 0  n % 3 == 0) {
+	    return false;
+    }    
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0  n % (i + 2) == 0) {
+		return false;
+	}	
     }
-    return NULL;
+    return true;
 }
 
-void sum(int n, int count) {
-    int arr[n];
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dis(1, 10); 
-    for (int i = 0; i < n; i++) { 
-	    arr[i] = dis(gen); 
-    }
-
-    ThreadClass th_datas[count];
-    int chunk = n / count;
-
-    std::clock_t clock_start = std::clock();
-
-    for (int i = 0; i < count; ++i) {
-        th_datas[i].arr = arr;
-        th_datas[i].start = i*chunk;
-        th_datas[i].end = (i == count - 1) ? n : (i + 1) * chunk;
-        int threadCreated = pthread_create(&th_datas[i].tid, NULL, ths_sum, (void*) &th_datas[i]); 
-        if(threadCreated != 0){
-            std::cerr << "error:cannot create new thread" << std::endl;
+void addPrimestoArr(int n, std::vector<int>& arr) {
+    for (int i = arr.back() + 1; arr.size() < n; ++i) {
+        if(checkIfPrime(i)) {
+            arr.push_back(i);
         }
     }
-    long int th_sum = 0;
-    for (int i = 0; i < count; ++i) {
-        pthread_join(th_datas[i].tid, NULL);
-        th_sum += th_datas[i].res;
-    }
-    std::clock_t clock_end = std::clock();
-    double duration = 1000.0 * (clock_end - clock_start) / CLOCKS_PER_SEC;
-    std::cout << "sum with threads took " << duration << " ms" << std::endl;
-    std::cout << th_sum <<  "\n";
-
-    clock_start = std::clock();
-    long int sum = 0; 
-    for (int i = 0; i < n; i++) { 
-        sum += arr[i];
-    }
-    clock_end = std::clock();
-    duration = 1000.0 * (clock_end - clock_start) / CLOCKS_PER_SEC;
-
-    std::cout << "sum without threads took " << duration << " ms" << std::endl;
-    std::cout << sum << std::endl;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cerr << "error: invalid arguments" << std::endl;
+
+int main() {
+    int rwPipefd[2];
+    int wrPipefd[2];
+    int rwPipe = pipe(rwPipefd);
+    int wrPipe = pipe(wrPipefd);
+    if (rwPipe == -1 || wrPipe == -1) {
+        std::cerr << "error: cannot create pipes" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    int n = atoi(argv[1]);
-    int m = atoi(argv[2]);
-    sum(n, m);
+    pid_t child = fork();
+    if(child == -1) {
+        std::cerr << "error:failed create a child" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if(child == 0) {
+        close(rwPipefd[0]);
+        close(wrPipefd[1]);
+        std::vector<int> arr = {2, 3, 5, 7};
+        while(true) {
+            char buff[buffSize];
+            int readBytes = read(wrPipefd[0], buff, buffSize);
+            if(readBytes == -1) {
+                std::cerr << "error to read file" << std::endl;
+                exit(EXIT_FAILURE);
+            } if(readBytes) {
+                buff[readBytes] = '\0';
+                int n = std::atoi(buff);
+                std::cout << "[Child] Calculating " << n << "-th prime number..." << std::endl;
+                if(n > arr.size()) {
+                    addPrimesToArr(n, arr);
+                }
+
+                std::cout << "[Child] Sending calculation result of prime(" << n << ")... " << std::endl;
+
+                std::string res = std::to_string(arr[n - 1]);
+                int writtenBytes = write(rwPipefd[1], res.c_str(), res.size());
+                if(writtenBytes == -1) {
+                    std::cerr << "error to write in file" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                break;
+            }
+        }
+        close(rwPipefd[1]);
+        close(wrPipefd[0]);
+    }
+    else if(child > 0) {
+        close(rwPipefd[1]);
+        close(wrPipefd[0]);
+        std::string m;
+        while(true){
+            char buff[buffSize] ;
+            std::cout << "[Parent] Please enter the number: ";
+            std::cin >> m;
+            if(m == "exit") {
+                close(wrPipefd[1]);
+                close(rwPipefd[0]);
+                wait(NULL);
+                break;
+            } else if(std::stoi(m) <= 0) {
+                std::cout << "invalid number" << std::endl;
+                continue;
+            }
+            std::cout << "[Parent] Sending " << m << " to the child process..." << std::endl;
+            std::cout << "[Parent] Waiting for the response from the child process...\n";
+            int writtenBytes = write(wrPipefd[1], m.c_str(), m.size());
+            if(writtenBytes == -1) {
+                std::cerr << "error to write in file" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            int readBytes = read(rwPipefd[0], buff, buffSize);
+            if(readBytes == -1) {
+                std::cerr << "error to read file" << std::endl;
+                exit(EXIT_FAILURE);
+            } else if (readBytes) {
+                buff[readBytes] = '\0';
+                int res = std::atoi(buff);
+                std::cout << "[Parent] Received calculation result of prime(" << m << ") = " << res << "...\n\n";
+            }
+        }
+    }
+
     return 0;
 }
